d5f19090fa8dd2dd2739893e4dd589ffa8e3a564
David Barrios <barriosadavid@gmail.com>
2019-12-05 12:35:42 -0700

    configuring settings for heroku

--- btre/settings.py
+++ btre/settings.py
@@ -131,4 +131,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
